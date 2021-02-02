56e79a204b77011e9940318b70c53037ab2ee37b
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-02-02 17:04:40 +0100

    Primer commit de los ejercicios UD3

---
+++ Ejercicios/src/EjercicosVarios/Ejercicio1.java
@@ -0,0 +1,22 @@
+package EjercicosVarios;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=6;
+		int b=20;
+		int suma=a+b;
+		int resta=a-b;
+		int multi=a*b;
+		int division=a%b;
+		
+		System.out.println("El total de la suma es:" +suma);
+		System.out.println("El total de la resta es:" +resta);
+		System.out.println("El total de la multiplicació es:" +multi);
+		System.out.println("El total de la división es:" +division);
+
+	}
+
+}
