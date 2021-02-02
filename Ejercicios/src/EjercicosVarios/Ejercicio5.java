052ea2dcdabe98428223364ac15674599a6d2190
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-02-02 19:57:53 +0100

    Commit del ejercicio 5

--- Ejercicios/src/EjercicosVarios/Ejercicio5.java
+++ Ejercicios/src/EjercicosVarios/Ejercicio5.java
@@ -4,9 +4,17 @@ public class Ejercicio5 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//act
 		
-		
+		int A=4;
+		int B=6;
+		int C=9;
+		int D=15;
+				
+		//System.out.println("Hola puta"+(A=C));
+		System.out.println("B toma el valor de C: " +C);
+		System.out.println("C toma el valor de A: "+A);
+		System.out.println("A toma el valor de D: "+D);
+		System.out.println("D toma el valor de B: "+B);
 	}
 
 }
